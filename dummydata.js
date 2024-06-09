4c17de7926638ea543d8058ad8c37f85e3202119
Mohit Anand <praveenanand801@gmail.com>
2024-06-09 16:13:59 +0530

    initial commit

---
+++ dummydata.js
@@ -0,0 +1,24 @@
+const tickets = [
+    {
+      category: 'Q1 2022',
+      color: 'red',
+      title: 'NFT Safety',
+      avatar: '',
+      status: 'done',
+      priority: 5,
+      progress: 40,
+      description: 'Make a video showcasing',
+      timestamp: '2022-02-11T07:36:17+0000'
+    },
+    {
+      category: 'Q1 2022',
+      color: 'red',
+      title: 'Build and sell',
+      avatar: '',
+      status: 'done',
+      priority: 2,
+      progress: 70,
+      description: 'Make a video showcasing',
+      timestamp: '2022-02-11T07:36:17+0000'
+    }
+  ]
